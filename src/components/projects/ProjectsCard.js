6c167f7eebbbd6b6735548525a70ebe74b3dad86
DEBRAJ GANGULY <dulalyganguly@gmail.com>
2025-06-27 19:29:50 +0530

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -37,7 +37,7 @@ const ProjectsCard = ({ title, des, src, githubLink, websiteLink }) => {
                   rel="noopener noreferrer"
                 >
                   <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                    <""/>
+                    <FaGlobe />
                   </span>
                 </a>
               )}
